85d2be0aef8297c441b33643b50cad5506d54c9c
Fakevin0613 <ke_qinyang@outlook.com>
2022-05-18 00:21:56 -0400

    yes

--- quote_generator/script.js
+++ quote_generator/script.js
@@ -1,23 +1,21 @@
 let apiQuotes = [];
+const abc = document.getElementById('new');
+abc.addEventListener('click', getQuotes);
 
-// Get Quotes From API
+function newq(){
+    quote = apiQuotes.joke;
+    document.getElementById('quotetext').textContent = quote;
+}
+
 async function getQuotes() {
-    const apiUrl = 'https://icanhazdadjoke.com/j/R7UfaahVfFd';
-    const response = await fetch("https://icanhazdadjoke.com/j/R7UfaahVfFd", {
+    const apiUrl = 'https://icanhazdadjoke.com/';
+    response = await fetch(apiUrl, {
             headers: {
-              Accept: "text/plain",
-              "User-Agent": "https://github.com/Fakevin0613/JavaScript-CSS-HTML"
+                Accept: 'Application/json'
             },
-           method: "POST"
           });
-    apiQuotes = await response.json();
-    console.log(apiQuotes);
-    // try{
-    //     
-    // }
-    // catch(error){
-    //     console.log("ok");
-    // }
+    apiQuotes = await response.json()
+    newq()
 }
 
 getQuotes();
