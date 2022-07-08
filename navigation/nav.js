fa17f30983ea163013bc6a4d6a2cfa8063640af4
Fakevin0613 <ke_qinyang@outlook.com>
2022-07-07 21:24:52 -0400

    add side bar

--- navigation/nav.js
+++ navigation/nav.js
@@ -1,5 +1,20 @@
 const navbutton = document.querySelector('.navother')
+let showSidebar = false;
 
 navbutton.addEventListener('click', function(){
-    this.classList.toggle('is_active')
+    showSidebar = !showSidebar
+    sideBarShow();
 })
+
+function sideBarShow(){
+    if(showSidebar){
+        navbutton.classList.toggle('is_active');
+        document.getElementById("mySideBar").style.width = "100px";
+        document.getElementById("mySideBar").style.marginLeft = "-100px";
+    }
+    else{
+        navbutton.classList.toggle('is_active');
+        document.getElementById("mySideBar").style.width = "0px";
+        document.getElementById("mySideBar").style.marginLeft = "0px";
+    }
+}
